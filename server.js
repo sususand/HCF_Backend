6bcdcea908b7e7b056d60b94b8ec0d92c0035d49
sususandi <sususandi.dev@gmail.com>
2024-06-24 20:40:14 +0630

    modify

--- server.js
+++ server.js
@@ -198,82 +198,6 @@ app.post("/upload-orders", (req, res) => {
   });
 });
 
-// // Handle file upload route
-// app.post("/upload-orders", (req, res) => {
-//   let insertedLength = 0;
-//   try {
-//     const records = req.body;
-//     console.log("Rescord length ", records.length);
-
-//     if (!records || !Array.isArray(records) || records.length === 0) {
-//       return res.status(400).json({ msg: "Invalid input data." });
-//     }
-
-//     const sql =
-//       "INSERT INTO orders (order_created_date, name, phone_number, address, items, price, delivery_fee, order_type, order_priority, prefer_time, prefer_shift) VALUES ?";
-//     const values = records.map((record) => [
-//       record.order_date
-//         ? moment(record.order_date).format("YYYY-MM-DD")
-//         : moment.tz("Asia/Yangon").format("YYYY-MM-DD"),
-//       record.name,
-//       record.phone_number,
-//       record.address,
-//       record.items,
-//       record.price,
-//       record.delivery_fee,
-//       record.order_type,
-//       record.order_priority,
-//       record.prefer_time,
-//       record.prefer_shift,
-//     ]);
-//     // Delete records with same date data
-//     const deleteQuery = "DELETE FROM orders WHERE order_created_date = ?";
-//     db.query(deleteQuery, [values[0][0]], (err, deleteResult) => {
-//       if (err) {
-//         console.error("Error deleting records:", err);
-//         return res
-//           .status(500)
-//           .json({ msg: "Failed to delete existing records." });
-//       }
-
-//       console.log(
-//         `Deleted ${deleteResult.affectedRows} existing records for date ${values[0][0]}.`
-//       );
-//     });
-
-//     //insert order data
-//     db.query(sql, [values], (err, result) => {
-//       if (err) {
-//         console.error(err);
-//         return res
-//           .status(500)
-//           .json({ msg: "Failed to insert! " + err.message });
-//       }
-//       insertedLength = result.affectedRows;
-//     });
-
-//     db.query(
-//       "select * from orders where order_created_date=? ORDER BY order_priority DESC;",
-//       values[0][0],
-//       (errRetrieve, recordsData) => {
-//         if (errRetrieve) {
-//           return res
-//             .status(500)
-//             .json({ msg: "Failed to recive inserted data! " + err.message });
-//         }
-//         return res.status(200).json({
-//           msg: "Number of records inserted: " + insertedLength,
-//           resultData: recordsData,
-//         });
-//       }
-//     );
-//   } catch (error) {
-//     console.log("Error : ", error);
-
-//     return res.status(500).json({ msg: `Error ${error}` });
-//   }
-// });
-
 app.post("/update-routes", (req, res) => {
   const { id, order_status } = req.body;
   db.query(
@@ -397,49 +321,6 @@ app.post("/plan-non-hala-route", (req, res) => {
     }
   });
 });
-
-// app.post("/plan-non-hala-route", (req, res) => {
-//   db.query("select * from trucks where truck_type='0'", (selectErr, trucks) => {
-//     if (trucks && trucks.length > 0) {
-//       let truck_count = trucks.length ?? 0;
-//       let order_count = 0;
-//       db.query(
-//         "SELECT COUNT(*) AS total_orders FROM orders WHERE order_status = '0' AND order_type = '0'",
-//         (err, result) => {
-//           order_count = result[0].total_orders;
-//           if (order_count === 0) {
-//             return res.status(404).json({
-//               msg: "All non-hala orders already planned.",
-//             });
-//           }
-//           let order_per_truck = Math.floor(order_count / truck_count);
-
-//           for (i = 0; i < truck_count; i++) {
-//             let limit = "";
-
-//             if (i !== truck_count - 1) {
-//               limit = "LIMIT " + order_per_truck;
-//             }
-//             db.query(
-//               "UPDATE orders SET truck_id = ?, order_status = 1 WHERE order_status=0 and order_type=0 " +
-//                 limit,
-//               [trucks[i].truck_id],
-//               (error, updated) => {
-//                 if (!error) {
-//                   return res.status(200).json({
-//                     msg: "Successfully planned non-hala route :" + order_count,
-//                   });
-//                 }
-//               }
-//             );
-//           }
-//         }
-//       );
-//     } else {
-//       return res.status(404).json({ msg: "There is no truck to plan." });
-//     }
-//   });
-// });
 
 // Start server
 app.listen(port, () => {
