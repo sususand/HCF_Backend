47478784d97706694b016237d9630d73e6d4d2a2
sususandi <sususandi.dev@gmail.com>
2024-06-24 20:16:43 +0630

    db update

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use an official Node runtime as a parent image
+FROM node:14-alpine
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json (if available)
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the entire project to the working directory
+COPY . .
+
+
+# Serve the app
+CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.use(express.urlencoded({ extended: false }));
 // MySQL database connection configuration
 const db = mysql.createConnection({
   host: "localhost",
-  user: "sususandi",
-  password: "password",
+  port: 3306,
+  user: "root",
+  password: "",
   database: "hcf_delivery",
 });
 
@@ -132,79 +133,147 @@ app.post("/update-truck", (req, res) => {
 
 // Handle file upload route
 app.post("/upload-orders", (req, res) => {
-  let insertedLength = 0;
-  try {
-    const records = req.body;
-    console.log("Rescord length ", records.length);
+  const records = req.body;
 
-    if (!records || !Array.isArray(records) || records.length === 0) {
-      return res.status(400).json({ msg: "Invalid input data." });
+  if (!records || !Array.isArray(records) || records.length === 0) {
+    return res.status(400).json({ msg: "Invalid input data." });
+  }
+
+  const values = records.map((record) => [
+    record.order_date
+      ? moment(record.order_date).format("YYYY-MM-DD")
+      : moment.tz("Asia/Yangon").format("YYYY-MM-DD"),
+    record.name,
+    record.phone_number,
+    record.address,
+    record.items,
+    record.price,
+    record.delivery_fee,
+    record.order_type,
+    record.order_priority,
+    record.prefer_time,
+    record.prefer_shift,
+  ]);
+
+  const deleteQuery = "DELETE FROM orders WHERE order_created_date = ?";
+  const insertQuery =
+    "INSERT INTO orders (order_created_date, name, phone_number, address, items, price, delivery_fee, order_type, order_priority, prefer_time, prefer_shift) VALUES ?";
+  const selectQuery =
+    "SELECT * FROM orders WHERE order_created_date = ? ORDER BY order_priority DESC";
+
+  db.query(deleteQuery, [values[0][0]], (err, deleteResult) => {
+    if (err) {
+      console.error("Error deleting records:", err);
+      return res
+        .status(500)
+        .json({ msg: "Failed to delete existing records." });
     }
 
-    const sql =
-      "INSERT INTO orders (order_created_date, name, phone_number, address, items, price, delivery_fee, order_type, order_priority, prefer_time, prefer_shift) VALUES ?";
-    const values = records.map((record) => [
-      record.order_date
-        ? moment(record.order_date).format("YYYY-MM-DD")
-        : moment.tz("Asia/Yangon").format("YYYY-MM-DD"),
-      record.name,
-      record.phone_number,
-      record.address,
-      record.items,
-      record.price,
-      record.delivery_fee,
-      record.order_type,
-      record.order_priority,
-      record.prefer_time,
-      record.prefer_shift,
-    ]);
-    // Delete records with same date data
-    const deleteQuery = "DELETE FROM orders WHERE order_created_date = ?";
-    db.query(deleteQuery, [values[0][0]], (err, deleteResult) => {
+    console.log(
+      `Deleted ${deleteResult.affectedRows} existing records for date ${values[0][0]}.`
+    );
+
+    db.query(insertQuery, [values], (err, result) => {
       if (err) {
-        console.error("Error deleting records:", err);
-        return res
-          .status(500)
-          .json({ msg: "Failed to delete existing records." });
+        console.error("Error inserting records:", err);
+        return res.status(500).json({ msg: "Failed to insert records." });
       }
 
-      console.log(
-        `Deleted ${deleteResult.affectedRows} existing records for date ${values[0][0]}.`
-      );
-    });
+      const insertedLength = result.affectedRows;
 
-    //insert order data
-    db.query(sql, [values], (err, result) => {
-      if (err) {
-        console.error(err);
-        return res
-          .status(500)
-          .json({ msg: "Failed to insert! " + err.message });
-      }
-      insertedLength = result.affectedRows;
-    });
-
-    db.query(
-      "select * from orders where order_created_date=? ORDER BY order_priority DESC;",
-      values[0][0],
-      (errRetrieve, recordsData) => {
-        if (errRetrieve) {
+      db.query(selectQuery, [values[0][0]], (err, recordsData) => {
+        if (err) {
+          console.error("Error retrieving records:", err);
           return res
             .status(500)
-            .json({ msg: "Failed to recive inserted data! " + err.message });
+            .json({ msg: "Failed to retrieve inserted data." });
         }
-        return res.status(200).json({
-          msg: "Number of records inserted: " + insertedLength,
+
+        res.status(200).json({
+          msg: `Number of records inserted: ${insertedLength}`,
           resultData: recordsData,
         });
-      }
-    );
-  } catch (error) {
-    console.log("Error : ", error);
-
-    return res.status(500).json({ msg: `Error ${error}` });
-  }
+      });
+    });
+  });
 });
+
+// // Handle file upload route
+// app.post("/upload-orders", (req, res) => {
+//   let insertedLength = 0;
+//   try {
+//     const records = req.body;
+//     console.log("Rescord length ", records.length);
+
+//     if (!records || !Array.isArray(records) || records.length === 0) {
+//       return res.status(400).json({ msg: "Invalid input data." });
+//     }
+
+//     const sql =
+//       "INSERT INTO orders (order_created_date, name, phone_number, address, items, price, delivery_fee, order_type, order_priority, prefer_time, prefer_shift) VALUES ?";
+//     const values = records.map((record) => [
+//       record.order_date
+//         ? moment(record.order_date).format("YYYY-MM-DD")
+//         : moment.tz("Asia/Yangon").format("YYYY-MM-DD"),
+//       record.name,
+//       record.phone_number,
+//       record.address,
+//       record.items,
+//       record.price,
+//       record.delivery_fee,
+//       record.order_type,
+//       record.order_priority,
+//       record.prefer_time,
+//       record.prefer_shift,
+//     ]);
+//     // Delete records with same date data
+//     const deleteQuery = "DELETE FROM orders WHERE order_created_date = ?";
+//     db.query(deleteQuery, [values[0][0]], (err, deleteResult) => {
+//       if (err) {
+//         console.error("Error deleting records:", err);
+//         return res
+//           .status(500)
+//           .json({ msg: "Failed to delete existing records." });
+//       }
+
+//       console.log(
+//         `Deleted ${deleteResult.affectedRows} existing records for date ${values[0][0]}.`
+//       );
+//     });
+
+//     //insert order data
+//     db.query(sql, [values], (err, result) => {
+//       if (err) {
+//         console.error(err);
+//         return res
+//           .status(500)
+//           .json({ msg: "Failed to insert! " + err.message });
+//       }
+//       insertedLength = result.affectedRows;
+//     });
+
+//     db.query(
+//       "select * from orders where order_created_date=? ORDER BY order_priority DESC;",
+//       values[0][0],
+//       (errRetrieve, recordsData) => {
+//         if (errRetrieve) {
+//           return res
+//             .status(500)
+//             .json({ msg: "Failed to recive inserted data! " + err.message });
+//         }
+//         return res.status(200).json({
+//           msg: "Number of records inserted: " + insertedLength,
+//           resultData: recordsData,
+//         });
+//       }
+//     );
+//   } catch (error) {
+//     console.log("Error : ", error);
+
+//     return res.status(500).json({ msg: `Error ${error}` });
+//   }
+// });
+
 app.post("/update-routes", (req, res) => {
   const { id, order_status } = req.body;
   db.query(
